fadd0bf6cb3d2b90362eeeeeacb6efcd55a25320
tqlab <redhat.jun@gmail.com>
2023-11-06 18:42:33 +0800

    fix

--- src/net.rs
+++ src/net.rs
@@ -166,18 +166,3 @@ impl Socket for MockSocket {
         None
     }
 }
-
-#[cfg(feature = "bench")]
-mod bench {
-    use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
-    use test::Bencher;
-
-    #[bench]
-    fn udp_send(b: &mut Bencher) {
-        let sock = UdpSocket::bind("127.0.0.1:0").unwrap();
-        let data = [0; 1400];
-        let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 1);
-        b.iter(|| sock.send_to(&data, &addr).unwrap());
-        b.bytes = 1400;
-    }
-}
