88fdb6ba90fa3c4179a3664b6a731dcfdbe8907c
tqlab <redhat.jun@gmail.com>
2023-11-20 19:25:49 +0800

    add logger

---
+++ src/logger.rs
@@ -0,0 +1,67 @@
+use std::{
+    fs::{self, File},
+    io::{self, Write},
+    path::Path,
+    sync::Mutex,
+};
+
+struct DualLogger {
+    file: Option<Mutex<File>>,
+}
+
+impl DualLogger {
+    pub fn new<P: AsRef<Path>>(path: Option<P>) -> Result<Self, io::Error> {
+        if let Some(path) = path {
+            let path = path.as_ref();
+            if path.exists() {
+                fs::remove_file(path)?
+            }
+            let file = File::create(path)?;
+            Ok(DualLogger { file: Some(Mutex::new(file)) })
+        } else {
+            Ok(DualLogger { file: None })
+        }
+    }
+}
+
+impl log::Log for DualLogger {
+    #[inline]
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    #[inline]
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            let time = chrono::Local::now().format("%F %H:%M:%S%.3f");
+
+            println!("{} - {} - {}", time, record.level(), record.args());
+            if let Some(ref file) = self.file {
+                let mut file = file.lock().expect("Lock poisoned");
+                writeln!(file, "{} - {} - {}", time, record.level(), record.args())
+                    .expect("Failed to write to logfile");
+            }
+        }
+    }
+
+    #[inline]
+    fn flush(&self) {
+        if let Some(ref file) = self.file {
+            let mut file = file.lock().expect("Lock poisoned");
+            try_fail!(file.flush(), "Logging error: {}");
+        }
+    }
+}
+
+pub fn init_logger(path: Option<&std::string::String>, verbose: bool, quiet: bool) {
+    let logger = try_fail!(DualLogger::new(path), "Failed to open logfile: {}");
+    log::set_boxed_logger(Box::new(logger)).unwrap();
+    assert!(!verbose || !quiet);
+    log::set_max_level(if verbose {
+        log::LevelFilter::Debug
+    } else if quiet {
+        log::LevelFilter::Error
+    } else {
+        log::LevelFilter::Info
+    });
+}
